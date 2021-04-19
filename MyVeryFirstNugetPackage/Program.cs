3952caf0d21b4f2d7fa8060a6c27c96690942652
Faith Olusegun <GD0024@fidelitybank.ng>
2021-04-19 01:53:06 +0100

    first build

---
+++ MyVeryFirstNugetPackage/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyVeryFirstNugetPackage
+{
+
+    public class Program
+    {
+
+        public static void Main(String[] args)
+        {
+            string text = "My name is Faith Emmanuel Olusegun (v1) onpoint";
+
+            string slugified = StringUtilities.Slugify(text);
+            Console.WriteLine("Slugified string = " + slugified);
+        }
+    }
+}
---
+++ MyVeryFirstNugetPackage/StringUtilities.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace MyVeryFirstNugetPackage
+{
+    public static class StringUtilities
+    {
+
+        ////public object Slugify { get; set; }
+
+        //private object Slugify;
+        ////{
+        ////    get { return this.myvalue; }
+        ////    set { this.myvalue = value; }
+        ////}
+
+        //public StringUtilsAttribute(object Slugify)
+        //{
+        //    this.Slugify = Slugify;
+        //}
+
+        //public override string ToString()
+        //{
+        //    return base.ToString() + ": " + GetType(Slugify).Equals();
+        //}
+
+        /// <summary>
+        /// This utility generates slug from any string property passed to it.
+        /// </summary>
+        /// <param name="phrase">string field to be slugified</param>
+        /// <returns>Slugified format of the input string</returns>
+        public static string Slugify(this string phrase)
+        {
+            string str = phrase.RemoveAccent().ToLower();
+            // invalid chars           
+            str = Regex.Replace(str, @"[^a-z0-9\s-]", "");
+            // convert multiple spaces into one space   
+            str = Regex.Replace(str, @"\s+", " ").Trim();
+            // cut and trim 
+            str = str.Substring(0, str.Length <= 45 ? str.Length : 45).Trim();
+            str = Regex.Replace(str, @"\s", "-"); // hyphens   
+            return str;
+        }
+
+        public static string RemoveAccent(this string txt)
+        {
+            byte[] bytes = Encoding.GetEncoding("Cyrillic").GetBytes(txt);
+            return Encoding.ASCII.GetString(bytes);
+        }
+
+    }
+}
